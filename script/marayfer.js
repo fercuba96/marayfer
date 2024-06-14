d50808e46c847be4e468fe745b537c3547bc4764
fercuba96 <fercuba96@gmail.com>
2024-06-14 11:52:11 -0500

    Listo en proceso

--- script/marayfer.js
+++ script/marayfer.js
@@ -30,3 +30,102 @@ function copiartres() {
     document.execCommand('copy');
     body.removeChild(copyFrom);
 }
+
+function copiarcuatro() {
+    var origen = document.getElementById('target5');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+}
+
+function copiarcinco() {
+    var origen = document.getElementById('target6');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+}
+
+function copiarseis() {
+    var origen = document.getElementById('target7');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+}
+
+function copiarsiete() {
+    var origen = document.getElementById('target8');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+}
+
+function copiarocho() {
+    var origen = document.getElementById('target9');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+}
+
+function copiarnueve() {
+    var origen = document.getElementById('target10');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+}
+
+function copiardiez() {
+    var origen = document.getElementById('target11');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+}
+
+function copiaronce() {
+    var origen = document.getElementById('target12');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+}
+
+function copiardoce() {
+    var origen = document.getElementById('target13');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+}
